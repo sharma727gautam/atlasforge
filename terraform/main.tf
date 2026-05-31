718734709977dbfedb20558b6927c558d46695ed
Gautam Sharma <sharma727gautam@gmail.com>
2026-05-31 12:25:16 +0530

    feat: create first Terraform infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,16 @@ terraform {
 provider "docker" {
 
 }
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "web" {
+  name  = "atlasforge-nginx"
+  image = docker_image.nginx.image_id
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
